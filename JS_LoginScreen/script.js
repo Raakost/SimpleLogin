27449d5aa526a07f97ab36534e0c0e28adcdcb3e
Raakost <jeanettesydboege@gmail.com>
2017-02-01 12:59:45 +0100

    .

---
+++ JS_LoginScreen/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.forgot-pw').click(function () {
+        alert("Too bad!");
+    });
+    $('#button').click(function () {
+        var username = "ib";
+        var password = "123";
+        var enteredUsername = $('.username').val().toLowerCase();
+        var enteredPassword = $('.password').val().toLowerCase();
+
+        if (enteredUsername == username && enteredPassword == password) {
+            alert("Logged in!");
+        } else {
+            alert("Wrong username or password!");
+        }
+    });
+});
